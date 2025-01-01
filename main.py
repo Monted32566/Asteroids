a42699ee468c7b3e1b3ff7408fae1799133e817f
Monted32566 <misiekmichalski2005@wp.pl>
2025-01-01 14:43:59 +0100

    adding black screen(corrected)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0), rect=None, special_flags=0)
+        screen.fill((0, 0, 0))
         pygame.display.flip()
 if __name__ == "__main__":
     main()
