ea3e7f68d79df1ab0c3f609c9e4b9b92c38a35b2
Monted32566 <misiekmichalski2005@wp.pl>
2025-01-01 16:05:04 +0100

    adding Player to the game

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill((0, 0, 0))
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        Fps = clock.tick(60)
+        dt = Fps / 1000
+
         pygame.display.flip()
+
 if __name__ == "__main__":
     main()
